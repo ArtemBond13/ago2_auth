e0f20f9bd225deff54ba2de6b1aafddf0f70ff0a
Artem <garribond@mail.ru>
2021-04-22 14:40:06 +0500

    Changed the file authenticator_test.go

--- middleware/pkg/middleware/authenticator/authenticator_test.go
+++ middleware/pkg/middleware/authenticator/authenticator_test.go
@@ -17,14 +17,56 @@ func TestAuthenticatorHTTPMux(t *testing.T) {
 	}, func(ctx context.Context, id *string) (interface{}, error) {
 		return "USERAUTH", nil
 	})
+
+	authenticatorFail := Authenticator(func(ctx context.Context) (*string, error) {
+		id := "192.0.2.1"
+		return &id, nil
+	}, func(ctx context.Context, id *string) (interface{}, error) {
+		return "FAIL", nil
+	})
+
 	mux.Handle(
 		"/get",
 		authenticatorMd(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 			profile, err := Authentication(request.Context())
 			if err != nil {
+				if err == ErrNoAuthentication {
+					writer.WriteHeader(http.StatusUnauthorized)
+					return
+				}
 				t.Fatal(err)
 			}
 			data := profile.(string)
+
+			if data != "USERAUTH" && request.RemoteAddr != "192.0.2.1" {
+				writer.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+
+			_, err = writer.Write([]byte(data))
+			if err != nil {
+				t.Fatal(err)
+			}
+		})),
+	)
+
+	mux.Handle(
+		"/post",
+		authenticatorFail(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+			profile, err := Authentication(request.Context())
+			if err != nil {
+				if err == ErrNoAuthentication {
+					writer.WriteHeader(http.StatusUnauthorized)
+					return
+				}
+				t.Fatal(err)
+			}
+			data := profile.(string)
+
+			if data != "USERAUTH" && request.RemoteAddr != "192.0.2.1" {
+				writer.WriteHeader(http.StatusUnauthorized)
+				return
+			}
 
 			_, err = writer.Write([]byte(data))
 			if err != nil {
@@ -36,15 +78,18 @@ func TestAuthenticatorHTTPMux(t *testing.T) {
 	type args struct {
 		method string
 		path   string
+		addr string
 	}
 
 	tests := []struct {
-		name string
-		args args
-		want []byte
+		name     string
+		args     args
+		want     []byte
+		wantCode int
 	}{
-		{name: "GET", args: args{method: "GET", path: "/get"}, want: []byte("USERAUTH")},
+		{name: "GET", args: args{method: "GET", path: "/get", addr: "192.0.2.1"}, want: []byte("USERAUTH"), wantCode: http.StatusOK},
 		// TODO: write for other methods
+		{name: "No auth", args: args{method: "POST", path: "/post", addr: "191.0.2.1"}, want: []byte{}, wantCode: http.StatusUnauthorized},
 	}
 
 	for _, tt := range tests {
@@ -54,6 +99,10 @@ func TestAuthenticatorHTTPMux(t *testing.T) {
 		got := response.Body.Bytes()
 		if !bytes.Equal(tt.want, got) {
 			t.Errorf("got %s, want %s", got, tt.want)
+		}
+		gotCode := response.Code
+		if gotCode != tt.wantCode {
+			t.Errorf("got %d, want %d", gotCode, tt.wantCode)
 		}
 	}
 }
@@ -71,9 +120,18 @@ func TestAuthenticatorChi(t *testing.T) {
 		func(writer http.ResponseWriter, request *http.Request) {
 			profile, err := Authentication(request.Context())
 			if err != nil {
+				if err != nil {
+					writer.WriteHeader(http.StatusUnauthorized)
+					return
+				}
 				t.Fatal(err)
 			}
 			data := profile.(string)
+
+			if data == "USERAUTH" && request.RemoteAddr != "192.0.2.1" {
+				writer.WriteHeader(http.StatusUnauthorized)
+				return
+			}
 
 			_, err = writer.Write([]byte(data))
 			if err != nil {
@@ -85,24 +143,33 @@ func TestAuthenticatorChi(t *testing.T) {
 	type args struct {
 		method string
 		path   string
+		addr string
 	}
 
 	tests := []struct {
 		name string
 		args args
 		want []byte
+		wantCode int
 	}{
-		{name: "GET", args: args{method: "GET", path: "/get"}, want: []byte("USERAUTH")},
+		{name: "GET", args: args{method: "GET", path: "/get"}, want: []byte("USERAUTH"), wantCode: http.StatusOK},
 		// TODO: write for other methods
+		{name: "No auth", args: args{method: "GET", path: "/get", addr: "191.0.2.1"}, want: []byte{}, wantCode: http.StatusUnauthorized},
+
 	}
 
 	for _, tt := range tests {
 		request := httptest.NewRequest(tt.args.method, tt.args.path, nil)
+		request.RemoteAddr = tt.args.addr
 		response := httptest.NewRecorder()
 		router.ServeHTTP(response, request)
 		got := response.Body.Bytes()
 		if !bytes.Equal(tt.want, got) {
 			t.Errorf("got %s, want %s", got, tt.want)
+		}
+		gotCode := response.Code
+		if tt.wantCode != gotCode{
+			t.Errorf("got %d, want %d", gotCode, tt.wantCode)
 		}
 	}
 }
